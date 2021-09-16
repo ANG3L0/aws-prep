e86d38ecafaed3a1dc76f80254d74a72904804c1
ANG3L0 <angelowo@umich.edu>
2021-09-16 11:58:59 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 python3 -m pip install pandas
 python3 -m pip install requests
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+sudo apt install nodejs
