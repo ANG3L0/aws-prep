4f670ba1b55479e12c13987ddef99443947e3024
ANG3L0 <angelowo@umich.edu>
2021-09-17 07:59:11 -0700

    Update install.sh
    beautiful soup

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 sudo dnf install python3-devel
 yum install alsa-lib-devel
+python3 -m pip install bs4
 source ~/.bashrc
