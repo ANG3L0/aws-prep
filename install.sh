1228783f57556e16de5c4aadd086256d320351d4
ANG3L0 <angelowo@umich.edu>
2021-09-17 12:52:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@ curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 python3 -m pip install pandas
 python3 -m pip install requests
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-sudo dnf install python3-devel
-yum install alsa-lib-devel
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - #not sure what this is.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash #nvm
+sudo dnf install python3-devel #dependency for simpleaudio
+yum install alsa-lib-devel #dependency for simpleaudio
 python3 -m pip install bs4
-curl https://intoli.com/install-google-chrome.sh | bash
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash - #npm
+sudo yum install -y nodejs #nodejs
 source ~/.bashrc
